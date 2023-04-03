d2bd27a75a2b4f41107de45b97a44e945a333a7c
Jen Cruz <maxcruz4455@gmail.com>
2023-04-03 11:42:22 +0800

    fixed some issues

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -108,7 +108,7 @@ exports.userChangePassword = async (req, res) => {
 
     if (value.current !== value.new) {
         req.flash('error', 'Password does not match with your new')
-        return res.status(400).redirect(`/inventor/user/password/${id}`)
+        return res.status(400).redirect(`/inventory/user/password/${id}`)
     }
 
     try {
